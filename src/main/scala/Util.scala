93a2b7b5653d8d4fb91524925aa2466fb04f8e2a
Yujian Zhang <yujian.zhang@gmail.com>
2014-08-12 19:31:37 +0800

    Add utility functions from worldmetro.

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -12,7 +12,11 @@
 package net.whily.scaland
 
 import android.app.Activity
+import android.content.{Context, SharedPreferences}
+import android.preference.PreferenceManager
+import android.app.Activity
 import android.view.View
+import android.widget.Toast
 
 object Util {
   /** Request immersive mode. */
@@ -24,5 +28,96 @@ object Util {
                             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                             View.SYSTEM_UI_FLAG_FULLSCREEN |
                             View.SYSTEM_UI_FLAG_IMMERSIVE)    
+  }
+
+  /** 
+   *  Return id (like R.raw.name). 
+   *  
+   *  @param activity: it is needed to access global variable Resources.
+   */
+  def getRawId(activity: Activity, name: String) =
+    getId(activity, name, "raw")
+    
+  /** 
+   *  Return id (like R.xml.name). 
+   *  
+   *  @param activity: it is needed to access global variable Resources.
+   */
+  def getXmlId(activity: Activity, name: String) =
+    getId(activity, name, "xml")    
+
+  /** 
+   *  Return string value corresponding to R.string.name. 
+   *  
+   *  @param activity: it is needed to access global variable Resources.
+   */    
+  def getString(activity: Activity, name: String) =
+    activity.getResources.getString(getStringId(activity, name))
+
+  /** Return id (like R.string.name). 
+   *  
+   *  @param activity: it is needed to access global variable Resources.
+   */    
+  def getStringId(activity: Activity, name: String) =
+    getId(activity, name, "string")    
+
+  /** Core function used by getRawId() or getStringId(). 
+   *  
+   *  @param defType indicates e.g. "raw" or "string". For details, see
+   *  android.content.res.Resources.getIdentifier().
+   */   
+  def getId(activity: Activity, name: String, defType: String) = {
+    activity.getResources.getIdentifier(name, defType, 
+      activity.getApplicationContext.getPackageName)
+  }
+
+  def toast (context: Context, text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+  }
+  
+  /** 
+   *  Return string value of the corresponding key; return default if not found. 
+   *  
+   *  @param activity is used to get the shared preference object.
+   */
+  def getSharedPref(activity: Activity, key: String, default: String) = 
+    PreferenceManager.getDefaultSharedPreferences(activity).getString(key, default)
+    
+  /** 
+   *  Set (key, value) to the shared preference object. 
+   *  
+   *  @param activity is used to get the shared preference object.
+   *  @param value String value
+   */
+  def setSharedPref(activity: Activity, key: String, value: String) {
+    val editor = PreferenceManager.getDefaultSharedPreferences(activity).edit()
+    editor.putString(key, value)
+    editor.commit()
+  }
+    
+  /** Return the theme preference in {0, 1}. */
+  def getThemePref(activty: Activity) = getSharedPref(activty, "theme_preference", "0").toInt
+  
+  /** Return the language preference. */
+  def getLanguagePref(activity: Activity) = 
+    getSharedPref(activity, "language_preference", "local")
+   
+  /**
+   * Return plural string.
+   * 
+   * @param activity: it is needed to access global variable Resources.
+   */
+  def getPluralString(activity: Activity, id: Int, quantity: Int) = {
+    val quantityInteger: Integer = quantity
+    activity.getResources.getQuantityString(id, quantity, quantityInteger)  
+  }
+  
+  /** Return text size for Edit component in unit of sp. */
+  def getEditTextSize(activity: Activity) = {
+    getSharedPref(activity, "text_size_preference", "1") match {
+      case "0" => 12
+      case "1" => 15
+      case "2" => 18
+    }    
   }
 }
